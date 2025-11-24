1be5df01e08c0e64702105b68e7b7f6f8868daf7
Jesse kariuki <jesseikariuki@gmail.com>
2025-11-24 23:02:06 +0300

    Add files via upload

---
+++ add-event.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+     <div>
+    <div class="container pt-3">
+        <a href="index.html" class="btn btn-primary"><i class="fa-solid fa-arrow-left"></i>Back</a>
+    </div>
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="container text-center">
+                    <h1 class="card-title m-b-0">Add New Event✝</h1>
+                </div>
+        <form>
+  <div class="mb-3">
+    <label for="exampleInputEvent" class="form-label">Event Name</label>
+    <input type="text" class="form-control" id="exampleInputEvent" aria-describedby="eventHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputdate" class="form-label">Date</label>
+    <input type="date" class="form-control" id="exampleInputdate" aria-describedby="dateHelp">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+            </div>
+            <div class="col-3"></div>
+
+</div>
+    </div>
+</body>
+</html>
